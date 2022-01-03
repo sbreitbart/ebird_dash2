9d7b1b156f2066f3b4ad5562bcf16df484ed3be2
Sophie Breitbart <sbreitbart@wesleyan.edu>
2022-01-03 11:25:03 -0500

    created rarest birds figure

--- ebird_dashboard2/app.R
+++ ebird_dashboard2/app.R
@@ -9,6 +9,9 @@ library(extrafont)
 library(scales)
 library(shinyWidgets)
 library(DT)
+library(reactable)
+library(htmltools)
+library(backports)
 
 
 # import data----
@@ -56,6 +59,126 @@ ebird_top20 <- ebird %>%
     head(20)
 
 
+ebird_singles <- ebird %>%
+    group_by(scientific_name.x) %>%
+    summarise(n = n(),
+              common_name = first(common_name),
+              location = first(location),
+              lat = first(latitude),
+              long = first(longitude),
+              date = first(date),
+              comments = first(checklist_comments),
+              details = first(observation_details)) %>%
+    filter(n == 1) %>%
+    # remove some observations only at the genus level
+    filter(!str_detect(scientific_name.x, 'sp.')) %>%
+    filter(!str_detect(scientific_name.x, 'Group')) %>%
+    dplyr::mutate(year = substr(date, 1, 4))
+
+
+# rarest birds table
+rarest_birds1 <- ebird_singles %>%
+    dplyr::select(c(1,3,4,7,9,10)) %>%
+    dplyr::rename("Common Name" = 2) %>%
+    dplyr::rename("Scientific Name" = 1) %>%
+    dplyr::rename("Location" = 3) %>%
+    dplyr::rename("Date" = 4) %>%
+    dplyr::rename("Comment" = 5) %>%
+    dplyr::rename("Year" = 6) %>%
+    dplyr::mutate("Link" = paste0(
+        "https://www.google.com/search?q=",
+        ebird_singles$common_name,
+        sep = " ")) %>%
+    dplyr::mutate(Date = as.Date(Date))
+
+                 
+ebird_monthly_all <- ebird %>%
+    tidyr::drop_na(date) %>%
+    dplyr::mutate(count_est = replace_na(count, 1)) %>%
+    dplyr::mutate(date = as.Date(date)) %>%
+    dplyr::mutate(count_est = as.numeric(count_est)) %>%
+    dplyr::group_by(date) %>%
+    dplyr::summarise(count_est = sum(count_est)) %>%
+    dplyr::mutate(month = lubridate::floor_date(date, "month")) %>%
+    dplyr::select(-date) %>%
+    dplyr::group_by(month) %>%
+    dplyr::summarise(count_est = sum(count_est)) %>%
+    dplyr::mutate(month_num = format(month,"%m")) %>%
+    dplyr::mutate(month_str = format(month,"%B")) %>%
+    dplyr::group_by(month_str, month_num) %>%
+    dplyr::summarise(total = sum(count_est)) %>%
+    arrange(month_num)
+
+
+
+monthly_birds <- ebird %>%
+    tidyr::drop_na(date) %>%
+    dplyr::mutate(count_est = replace_na(count, 1)) %>%
+    dplyr::mutate(date = as.Date(date)) %>%
+    dplyr::mutate(count_est = as.numeric(count_est)) %>%
+    dplyr::group_by(date) %>%
+    dplyr::summarise(count_est = sum(count_est)) %>%
+    dplyr::mutate(month = lubridate::floor_date(date, "month")) %>%
+    dplyr::select(-date) %>%
+    dplyr::mutate(month_num = format(month,"%m")) %>%
+    dplyr::mutate(month_str = format(month,"%B")) %>%
+    ggplot( aes(x = month_str,
+                y = count_est,
+                fill = month_str,
+                group = month_str)) +
+    geom_boxplot() +
+    scale_x_discrete(limits = month.name) +
+    geom_jitter(aes(fill = month_str), color = "black", shape = 21, size=1.2, alpha=0.9) +
+    theme_bw() +
+    theme(
+        legend.position="none",
+        plot.title = element_text(size=11)
+    ) +
+    scale_fill_brewer(type = "qual", palette = "Set3") +
+    xlab("") +
+    labs(y = "Birds Seen per Outing")
+
+
+# best comments
+ebird_comments <- ebird %>%
+    dplyr::filter(observation_details != "") %>%
+    dplyr::slice(c(9, 20, 21, 22, 34, 40, 48, 58, 59, 66, 68, 78, 81, 84, 86, 105, 108, 110, 113, 115, 117, 122))
+
+
+leaflet(ebird_comments) %>% 
+    # addTiles(options = providerTileOptions(opacity = 0.55)) %>%
+    addProviderTiles(providers$CartoDB.Positron) %>%
+    
+    #                  options = providerTileOptions(opacity = 0.55)) %>%
+    # fitBounds(-79.6,44,-79.2,43.5) %>% 
+    addCircles(lng = ~longitude,
+               lat = ~latitude,
+               radius = 20,
+               popup = paste0("Scientific name: ",
+                              # italicize
+                              "<i>",
+                              ebird_comments$scientific_name.x,
+                              "</i>",
+                              ".",
+                              "<br>", # line break
+                              "Common name: ",
+                              ebird_comments$common_name, 
+                              ".",
+                              "<br>", # line break
+                              "Location: ",
+                              ebird_comments$location,
+                              ".",
+                              "<br>", # line break
+                              "Date: ",
+                              ebird_comments$date,
+                              ".",
+                              "<br>", # line break
+                              "Comment: ",
+                              ebird_comments$observation_details,
+                              "."),
+               fill = T,
+               fillOpacity = 0.8
+    ) 
 
 
 
@@ -120,16 +243,15 @@ ui <- fluidPage(
                      plotOutput("common20")),
             
             tabPanel("Rarest Birds",
-                     plotOutput("LifebirdsState")),
+                     fluidPage(
+                     reactableOutput("rarest_birds"))),
             
             tabPanel("Sightings by Month",
-                     plotOutput("birdsByMonth")),
+                     plotOutput("monthly_birds")),
             
             tabPanel("Sightings with the Best Comments",
-                     plotOutput('birdsByYear'))
-            
-            # leafletOutput("plot")
-            )  # Inside the sidebarLayout, add a mainPanel
+                     plotOutput('bird_comments'))
+                        ) 
     ))
 )
 
@@ -309,11 +431,36 @@ server <- function(input, output, session) {
             geom_text(aes(label = count),
                       position = position_dodge(width=0.9),
                       vjust = -0.8)
+    
     )
+
+    output$rarest_birds <- renderReactable({
+        reactable::reactable(rarest_birds1,
+                             groupBy = "Year",
+                             columns = list(
+                                 Link = colDef(html = FALSE,
+                    cell = function(value) {
+                    htmltools::tags$a(href = value,
+                                      target = "_blank",
+                                      value)})                    ),
+                    highlight = TRUE
+                    
+                    )
+        })
+ 
+    # birds seen per month
+    output$monthly_birds <- renderPlot(
+        monthly_birds)
     
     
+    # best comments
     
+       
 }
+
+
+
+
 
 
 shinyApp(ui, server)
